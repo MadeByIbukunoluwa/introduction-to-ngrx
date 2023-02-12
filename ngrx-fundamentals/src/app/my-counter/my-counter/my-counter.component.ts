d1b97fedc90c1f193671247d55109f2c7a0d724d
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-12 11:04:00 +0100

    feat:added logic to dispatch actions , injected global store in the counter-component constructor

--- ngrx-fundamentals/src/app/my-counter/my-counter/my-counter.component.ts
+++ ngrx-fundamentals/src/app/my-counter/my-counter/my-counter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { observable, Observable } from 'rxjs';
+import {Store} from '@ngrx/store'
+import { increment,decrement,reset } from 'src/app/counter-actions/counter-actions.component';
 
 @Component({
   selector: 'app-my-counter',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-counter.component.scss']
 })
 export class MyCounterComponent {
-
+count$ : Observable<number> = new Observable<number>()
+constructor(private store:Store<{count:number}>){
+  // Connect this this.$count to the current store `count` state
+  this.count$ = store.select('count')
+}
+ increment() {
+    // Dispatch an action that decrements 
+    this.store.dispatch(increment())
+ }
+ decrement(){
+    // Dispatch an action that increments 
+    this.store.dispatch(decrement())
+ }
+ reset() {
+    // Dispatch an action that resets to 0
+    this.store.dispatch(reset())
+ }
 }
