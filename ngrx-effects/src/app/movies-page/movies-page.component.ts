7831b5c4c203e302966a0c371d8a27eef3916ddd
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-02-12 14:13:19 +0100

    styling:addded poppins font and modified styles

--- ngrx-effects/src/app/movies-page/movies-page.component.ts
+++ ngrx-effects/src/app/movies-page/movies-page.component.ts
@@ -5,11 +5,10 @@ import { Movie, Results } from '../movie';
   selector: 'app-movies-page',
   template: `<li *ngFor="let movie of results">
     <h1>{{ movie.title }}</h1>
-    {{ movie.episode_id }}
-    {{ movie.episode_id }}
-    {{ movie.opening_crawl }}
-    {{ movie.director }}
-    {{ movie.release_date }}
+    <p>{{ movie.episode_id }}</p>
+    <p>{{ movie.opening_crawl }}</p>
+    <p>{{ movie.director }}</p>
+    <p>{{ movie.release_date }}</p>
   </li> `,
   styleUrls: ['./movies-page.component.scss'],
 })
@@ -18,25 +17,27 @@ export class MoviesPageComponent implements OnInit {
   results!: Results[];
   constructor(private movieService: MovieService) {}
   ngOnInit(): void {
-    this.movieService.getAll().subscribe((movies) => this.results = movies.results.map(
-      ({
-        title,
-        episode_id,
-        opening_crawl,
-        director,
-        producer,
-        release_date,
-      }) => {
-        return {
-          title,
-          episode_id,
-          opening_crawl,
-          director,
-          producer,
-          release_date,
-        };
-      }
-    ))
-
+    this.movieService.getAll().subscribe(
+      (movies) =>
+        (this.results = movies.results.map(
+          ({
+            title,
+            episode_id,
+            opening_crawl,
+            director,
+            producer,
+            release_date,
+          }) => {
+            return {
+              title,
+              episode_id,
+              opening_crawl,
+              director,
+              producer,
+              release_date,
+            };
+          }
+        ))
+    );
   }
 }
